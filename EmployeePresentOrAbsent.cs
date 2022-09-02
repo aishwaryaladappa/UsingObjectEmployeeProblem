47256ff71a5464744021b14d63454e8528c5b346
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-02 17:57:17 +0530

    added EmployeePresentOrAbsent.cs

---
+++ EmployeePresentOrAbsent.cs
@@ -0,0 +1,30 @@
+﻿
+        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWageProblem
+    {
+        public  class EmployeePresentOrAbsent
+        {
+            public  void Attendance()
+            {
+                int IS_FULL_TIME = 1;
+                Random random = new Random();
+                int empCheck = random.Next(2);
+                if (empCheck == IS_FULL_TIME)
+
+                {
+                    Console.WriteLine("employee is present");
+                }
+                else
+                {
+                    Console.WriteLine("employee is absent");
+                }
+
+            }
+        }
+    }
+
+
