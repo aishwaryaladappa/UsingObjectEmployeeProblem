9b54dd009ba94fa08f06612e754b07bd4f05faa7
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-02 17:53:53 +0530

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeeWageProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeePresentOrAbsent obj = new EmployeePresentOrAbsent();
+            obj.Attendance();
+            EmployeeWage obj1 = new EmployeeWage();
+            obj1.Wage();
+            Parttime obj2 = new Parttime();
+            obj2.EmpParttime();
+            ForTwentyDays obj4 = new ForTwentyDays();
+            obj4.EmpWageSwitch();
+            Switchcase obj5 = new Switchcase();
+            obj5.EmpWageSwitch();
+            TotalWorkingHourPerMonth totalWorkingHourPerMonth = new TotalWorkingHourPerMonth();
+            totalWorkingHourPerMonth.TotalWorkingHrs();
+
+
+        }
+    }
+}
