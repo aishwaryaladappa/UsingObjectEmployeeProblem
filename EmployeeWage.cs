808a34a058ec0c19edf2a9790b517a1f7f65721f
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-02 17:58:50 +0530

    added EmployeeWage.cs

---
+++ EmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public  class EmployeeWage
+
+    {
+        public  void Wage()
+        {
+            int IS_FULL_TIME = 1;
+            int EmpHrs = 0;
+            int Empwage = 0;
+            int Emp_RATE_PER_HOUR = 20;
+            Random random = new Random();
+            int EmpCheck = random.Next(2);
+            if (EmpCheck == IS_FULL_TIME)
+            {
+                EmpHrs = 8;
+            }
+            else
+            {
+                EmpHrs = 0;
+
+            }
+            Empwage = EmpHrs * Emp_RATE_PER_HOUR;
+            Console.WriteLine("EmpWage:" + Empwage);
+
+
+        }
+    }
+}
